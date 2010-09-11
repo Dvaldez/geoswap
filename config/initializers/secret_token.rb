83fde9c555d73e038845488d1ce218b8dfc65364
D Valdez <daveavaldez@gmail.com>
2010-09-11 09:55:59 -1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoswap::Application.config.secret_token = 'da198ea03059f649bf9552e7e504fc12a3991b0aa5955e40e3d713f779268f43ea73ce9c4582eb117dac873b84e7ce24305e7a4e6380041e7d97914985e382cf'
